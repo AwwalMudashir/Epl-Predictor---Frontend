5be36bc2a5929b3a5e1c30f0eeac1de553487990
AwwalMudashir <awwalmudashir2.0@gmail.com>
2026-03-02 11:44:17 +0100

    login and register blend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
+  baseURL: 'https://epl-prediction-app-backend.onrender.com' || 'http://localhost:8080',
 });
 
 // logging interceptor
