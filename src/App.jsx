b0ea4e7875c06c5ee26b600c55146a27f182af90
AwwalMudashir <awwalmudashir2.0@gmail.com>
2026-03-02 13:03:41 +0100

    login and register blend

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     <div className="min-h-screen bg-gray-light">
       <Navbar />
       {/* push content down so fixed navbar doesn't overlap */}
-      <main className="mt-12 pt-8">
+      <main className="mt-12 pt-12">
         <Routes>
           <Route path="/login" element={!user ? <Login /> : <Navigate to="/dashboard" />} />
           <Route path="/register" element={!user ? <Register /> : <Navigate to="/dashboard" />} />
